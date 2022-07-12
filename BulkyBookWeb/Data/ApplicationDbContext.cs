774297e3d38baf15bd987dcdbdd391f8e61f9679
tothenextcode <tothenextcode@gmail.com>
2022-07-12 09:20:43 -0600

    Install EntityFrameworkCore and create ApplicationDbContext. Refactor Category data annotations to Fluent API.

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasKey(v => v.Id);
+
+            modelBuilder.Entity<Category>()
+                .Property(v => v.Name)
+                .IsRequired();
+        }
+    }
+}
